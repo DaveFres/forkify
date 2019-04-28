834dd01b11bc98642139f6fc8d0202c22b8d9a67
DaveFres <nanyandave@gmail.com>
2019-04-28 20:16:12 +0300

    Added limit to the recipes titles

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -3,12 +3,28 @@ import { elements } from "./base";
 export const getInput = () => elements.searchInput.value;
 
 export const clearInput = () => {
-    elements.searchInput.value = '';
+    elements.searchInput.value = "";
 };
 
 export const clearResults = () => {
-    elements.searchResList.innerHTML = '';
-}
+    elements.searchResList.innerHTML = "";
+};
+
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(" ").reduce((acc, cur) => {
+            if (acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0);
+
+        // return the result
+        return `${newTitle.join(" ")} ...`;
+    }
+    return title;
+};
 
 const renderRecipe = recipe => {
     const markup = `
@@ -18,13 +34,15 @@ const renderRecipe = recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(
+                        recipe.title
+                    )}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
         </li>
     `;
-    elements.searchResList.insertAdjacentHTML('beforeend', markup);
+    elements.searchResList.insertAdjacentHTML("beforeend", markup);
 };
 
 export const renderResults = recipes => {
